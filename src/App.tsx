0049732cc6e2a769bfe8a9e195e1388da4a90405
Daniil Alabuzhev <90332154+Kuroky79@users.noreply.github.com>
2023-01-16 02:48:44 +0300

    commit posts (remove and add)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-import React, {useEffect, useState} from 'react';
-import './App.css'
-import {useDispatch, useSelector} from "react-redux";
-import {useAppDispatch, useAppSelector} from "./hooks/redux";
+import React, {useEffect} from 'react';
+import './App.css';
 import {userSlice} from "./store/reducers/UserSlice";
+import {useDispatch} from "react-redux";
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
 import {fetchUsers} from "./store/reducers/ActionCreators";
+import PostContainer from "./components/PostContainer";
+import PostContainer2 from "./components/PostContainer2";
 
-const App = () => {
-    // const {count} = useAppSelector(state => state.userReducer)
-    // const {increment} = userSlice.actions;
-    const {users ,isLoading, error } = useAppSelector(state => state.userReducer)
-    const dispatch = useAppDispatch()
-    useEffect(()=>{
-        dispatch(fetchUsers())
-    }, [])
+function App() {
+    // const dispatch = useAppDispatch()
+    // const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+    //
+    // useEffect(() => {
+    //     dispatch(fetchUsers())
+    // }, [ ])
+
     return (
-        <div className='App'>
-            {isLoading && <h3>Идет загрузка...</h3>}
-            {error && <h3>{error}</h3>}
-            {JSON.stringify(users, null,2)}
-            {/*<h1>{count}</h1>*/}
-            {/*<button onClick={()=>dispatch(increment(1))}>PLUS</button>*/}
+        <div className="App">
+            {/*{isLoading && <h1>Идет загрузка...</h1>}*/}
+            {/*{error && <h1>{error}</h1>}*/}
+            {/*{JSON.stringify(users, null, 2)}*/}
+            <div style={{display: 'flex'}}>
+                <PostContainer/>
+                <PostContainer2/>
+            </div>
         </div>
     );
-};
+}
 
 export default App;
--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -4,25 +4,24 @@ import {IUser} from "../../models/IUser";
 import {userSlice} from "./UserSlice";
 import {createAsyncThunk} from "@reduxjs/toolkit";
 
-
-
-
-// export const fetchUsers = () => async (dispatch: AppDispatch) => { // не возвращфем сразу action а возвращаем функцию которая аргументом забирает dispatch
-//     try{
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//     try {
 //         dispatch(userSlice.actions.usersFetching())
 //         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
 //         dispatch(userSlice.actions.usersFetchingSuccess(response.data))
-//     }catch (e){
-//         // @ts-ignore
-//         dispatch(userSlice.actions.usersFetchingError(e.message));
+//     } catch (e) {
+//         dispatch(userSlice.actions.usersFetchingError(e.message))
 //     }
 // }
 
-
 export const fetchUsers = createAsyncThunk(
     'user/fetchAll',
-    async (_,thunkAPI) =>{
-        const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
-        return response.data
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/user2s')
+            return response.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue("Не удалось загрузить пользователей")
+        }
     }
 )
